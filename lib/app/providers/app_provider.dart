4db9547dd39efafcb685d783b3bb68289035b3bc
Adrian Muñoz <eladrixhd@gmail.com>
2025-06-17 16:07:27 +0200

    feat: update app branding with new icons and launch screens
    - Update app icons for Android and iOS platforms
    - Add dark mode support for launch screens
    - Update launch backgrounds and splash screens
    - Modify app provider and dependencies
    - Update project logo and branding assets

--- lib/app/providers/app_provider.dart
+++ lib/app/providers/app_provider.dart
@@ -1,12 +1,13 @@
-import '/config/keys.dart';
+import 'package:nylo_framework/nylo_framework.dart';
+
 import '/app/forms/style/form_style.dart';
-import '/config/form_casts.dart';
 import '/config/decoders.dart';
 import '/config/design.dart';
+import '/config/form_casts.dart';
+import '/config/keys.dart';
+import '/config/localization.dart';
 import '/config/theme.dart';
 import '/config/validation_rules.dart';
-import '/config/localization.dart';
-import 'package:nylo_framework/nylo_framework.dart';
 
 class AppProvider implements NyProvider {
   @override
